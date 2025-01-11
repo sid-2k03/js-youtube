da476bb6576f22244a4e094ca90d33029f8e5f6c
sid-2k03 <explisid@gmail.com>
2025-01-11 11:18:04 +0000

    conversion of data types

---
+++ practice/02datatypes.js
@@ -0,0 +1,24 @@
+console.log(3 + 3)
+console.log("Sid")
+
+let name = "Sid"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+
+
+//object
+
+console.log(typeof "sid")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+
